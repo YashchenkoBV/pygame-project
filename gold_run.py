8a296d4d64944e50895e042a15d983328922755f
yashchenko <ya.bogdan1606@gmail.com>
2022-01-16 23:36:47 +0300

    повторная генерация уровня, счет монет

--- gold_run.py
+++ gold_run.py
@@ -1,7 +1,12 @@
 import pygame
 import sys
 import os
+import sqlite3
 from PIL import Image
+
+username = '8'
+fullname = os.path.join('data', 'user.db')
+
 
 
 def load_image(name):
@@ -19,37 +24,39 @@ FPS = 30
 size = WIDTH, HEIGHT = 700, 800
 clock = pygame.time.Clock()
 tile_width = tile_height = 50
-STEP = 4
+STEP = 5
 ALIVE = True
 WIN = False
+won = False
 ground_y = set()
 cur_coins = 0
 all_coins = 0
 
 tileset_1 = load_image('morning_adventures_tileset_16x16.png')
 ground_island = pygame.Surface.subsurface(tileset_1, (50, 48, 12, 16))
-spikes = pygame.Surface.subsurface(tileset_1, (80, 80, 16, 16))
 ground_island = pygame.transform.scale(ground_island, (50, 50))
+spikes = pygame.Surface.subsurface(tileset_1, (80, 80, 16, 16))
 spikes = pygame.transform.scale(spikes, (50, 40))
 
 player_width, player_height = Image.open('data/run2.png').size
 
-cur_state = 'menu'
+cur_state = 'enter'
 levels_lst = []
 button_message_window_win = pygame.Rect(160, 390, 380, 100)
 
 all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
+jumping_player_group = pygame.sprite.Group()
+level_group = pygame.sprite.Group()
 background_group = pygame.sprite.Group()
-spikes_group = pygame.sprite.Group()
-chests_group = pygame.sprite.Group()
 coins_group = pygame.sprite.Group()
 
 
 def start_screen():
+    global cur_state
+    global screen, size, app, form
     intro_text = ["Это временно"]
-
+    screen = pygame.display.set_mode(size)
     fon = pygame.transform.scale(load_image('test_fon.png'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
@@ -64,15 +71,15 @@ def start_screen():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                cur_state = 'menu'
+                return
         pygame.display.flip()
         clock.tick(FPS)
 
 
 class Sprites(pygame.sprite.Sprite):
-    def __init__(self, image, group, pos_x, pos_y, x_move=0, y_move=0):
+    def __init__(self, image, pos_x, pos_y, x_move=0, y_move=0, group=level_group):
         super().__init__(group, all_sprites)
         self.image = image
         self.rect = self.image.get_rect().move(tile_width * pos_x + x_move, tile_height * pos_y + y_move)
@@ -123,27 +130,38 @@ def generate_level(level):
     spikes_lst = []
     chests_lst = []
     coins_lst = []
+    tiles_lst = []
     for y in range(len(level)):
         for x in range(len(level[y])):
             if level[y][x] == '.':
                 continue
             elif level[y][x] == '#':
-                spike = Sprites(pygame.transform.scale(spikes, (50, 50)), spikes_group, x, y, y_move=1)
+                spike = Sprites(pygame.transform.scale(spikes, (50, 50)), x, y, y_move=1)
                 spikes_lst.append(spike)
             elif level[y][x] == 'C':
-                chest = Sprites(pygame.transform.scale(load_image('chest.png'), (60, 50)), chests_group, x, y)
+                chest = Sprites(pygame.transform.scale(load_image('chest.png'), (60, 50)), x, y)
                 chests_lst.append(chest)
             elif level[y][x] == '@':
                 new_player = AnimatedSprite(load_image('run1.png'), 32, 1, x, y, y_move=9)
             elif level[y][x] == '_':
-                tile = Sprites(pygame.transform.scale(ground_island, (50, 50)), tiles_group, x, y)
+                tile = Sprites(pygame.transform.scale(ground_island, (50, 50)), x, y)
+                tiles_lst.append(tile)
                 global ground_y
                 ground_y.add(tile.rect.y - player_height)
             elif level[y][x] == '!':
                 coin = AnimatedSprite(pygame.transform.scale(load_image('coin.png'), (560, 20)), 14, 1, x, y, 8, -20)
                 coins_lst.append(coin)
+            elif level[y][x] == 'T':
+                stone = Sprites(pygame.transform.scale(load_image('tree.png'), (100, 150)), x, y, y_move=-100)
+                spikes_lst.append(stone)
+            elif level[y][x] == '*':
+                bush = Sprites(pygame.transform.scale(load_image('bush.png'), (100, 40)), x, y, x_move=-50, y_move=10)
+                spikes_lst.append(bush)
+            elif level[y][x] == 't':
+                tree = Sprites(pygame.transform.scale(load_image('tree.png'), (100, 150)), x, y, y_move=-100)
+                chests_lst.append(tree)
     # вернем игрока, а также размер поля в клетках
-    return new_player, x, y, spikes_lst, chests_lst, coins_lst
+    return new_player, x, y, spikes_lst, chests_lst, coins_lst, tiles_lst
 
 
 class Camera:
@@ -173,9 +191,9 @@ def terminate():
 
 
 def jump():
-    y_pos = player.rect.y
-    jump_h = ground - 90
-    global down_f
+    global down_f, jump_f, player
+    y_pos = jumping_player.rect.y
+    jump_h = ground - 80
     if y_pos > jump_h and not down_f:
         y_pos -= 4  # v * t в секундах
     if y_pos <= jump_h:
@@ -184,11 +202,11 @@ def jump():
     if down_f:
         y_pos += 4  # v * t в секундах
     if y_pos >= ground:
-        global jump_f
         jump_f = False
         y_pos = ground
-    player.rect.y = y_pos
-    player.rect.x += 4
+        player.rect.x, player.rect.y = jumping_player.rect.x, ground
+    jumping_player.rect.y = y_pos
+    jumping_player.rect.x += STEP
 
 
 def message_window_win():
@@ -227,23 +245,60 @@ def message_window_win():
     clock.tick(FPS)
 
 
+def message_window_lose():
+    col = pygame.Color('#640933')
+    pygame.draw.rect(screen, col, (150, 250, 400, 250), 0)
+    text = ['Вы умерли', 'Попытайтесь в следующий раз!']
+    text_coord = 250
+    font = pygame.font.Font(None, 30)
+    for line in text:
+        string_rendered = font.render(line, True, pygame.Color('yellow'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 155
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    global button_message_window_win
+    pygame.draw.rect(screen, pygame.Color('#ff7029'), button_message_window_win, 0)
+    button_text = 'Вернуться в меню'
+    font_button = pygame.font.Font(None, 60)
+    string_rendered = font_button.render(button_text, True, pygame.Color('yellow'))
+    intro_rect = string_rendered.get_rect()
+    intro_rect.top = 415
+    intro_rect.x = 160
+    screen.blit(string_rendered, intro_rect)
+
+
 def main_menu():
+    global cur_state
     global WIDTH, HEIGHT
-    global all_coins, cur_coins
+    global all_coins, cur_coins, username, fullname
     global player, level_x, level_y, spikes_lst, chests_lst, coins_lst
     pygame.display.set_caption('МЕНЮ')
     col1 = pygame.Color('#640933')
     pygame.draw.rect(screen, col1, (0, 0, WIDTH, HEIGHT), 0)
-
+    con = sqlite3.connect(fullname)
+    cur = con.cursor()
+    if won:
+        cur.execute(
+            '''UPDATE user_info SET coins = ?''',
+            (all_coins + cur_coins,))
+        con.commit()
     main_font = pygame.font.Font(None, 120)
     title = main_font.render('Gold Run', True, pygame.Color('yellow'))
     title_rect = title.get_rect()
     title_rect.top = 20
     title_rect.x = 155
     screen.blit(title, title_rect)
-
+    result = cur.execute("""SELECT coins FROM user_info
+                                WHERE name = ?""", (username,)).fetchall()
+    if result[0][0] is None:
+        res = 0
+    else:
+        res = result[0][0]
     coins_font = pygame.font.Font(None, 30)
-    coins = coins_font.render(f'Монеты: {all_coins + cur_coins}', True, pygame.Color('yellow'))
+    coins = coins_font.render(f'Монеты: {res}', True, pygame.Color('yellow'))
     coins_rect = coins.get_rect()
     coins_rect.top = 110
     coins_rect.x = 10
@@ -275,10 +330,10 @@ def main_menu():
                 for elem in levels_lst:
                     c += 1
                     if elem.collidepoint(mouse_pos) and cur_state == 'menu':
-                        player, level_x, level_y, spikes_lst, chests_lst, coins_lst = None, None, None,\
-                                                                                      None, None, None
+                        player, level_x, level_y, spikes_lst, chests_lst, coins_lst = [None] * 6
                         all_coins += cur_coins
                         cur_coins = 0
+                        cur_state = 'game'
                         return generate_level(load_level(f'lvl{c}.txt'))
 
         pygame.display.flip()
@@ -287,7 +342,8 @@ def main_menu():
 
 screen = pygame.display.set_mode(size)
 start_screen()
-player, level_x, level_y, spikes_lst, chests_lst, coins_lst = main_menu()
+player, level_x, level_y, spikes_lst, chests_lst, coins_lst, tiles_lst = main_menu()
+jumping_player = Sprites(load_image('jump1.png'), player.rect.x, player.rect.y, group=jumping_player_group)
 camera = Camera((level_x, level_y))
 running = True
 jump_f = False
@@ -301,34 +357,51 @@ while running:
             mouse_pos = event.pos
             if button_message_window_win.collidepoint(mouse_pos) and cur_state == 'win':
                 cur_state = 'menu'
-                player, level_x, level_y, spikes_lst, chests_lst, coins_lst = main_menu()
-                camera = Camera((level_x, level_y))
                 ALIVE = True
-                print('okh')
-
+                level_group.empty()
+                coins_group.empty()
+                player_group.empty()
+                player, level_x, level_y, spikes_lst, chests_lst, coins_lst, tiles_lst = main_menu()
+                camera = Camera((level_x, level_y))
+                won = False
+            elif button_message_window_win.collidepoint(mouse_pos) and cur_state == 'dead_inside':
+                cur_state = 'menu'
+                ALIVE = True
+                level_group.empty()
+                coins_group.empty()
+                player_group.empty()
+                player, level_x, level_y, spikes_lst, chests_lst, coins_lst, tiles_lst = main_menu()
+                camera = Camera((level_x, level_y))
+                won = False
         elif event.type == pygame.KEYDOWN and ALIVE:
             if event.key == pygame.K_UP and player.rect.y > sorted(list(ground_y))[0] and not jump_f:
                 player.rect.y -= sorted(list(ground_y))[1] - sorted(list(ground_y))[0]
+                print(player.rect.y)
             if event.key == pygame.K_DOWN and player.rect.y < sorted(list(ground_y))[2] and not jump_f:
                 player.rect.y += sorted(list(ground_y))[1] - sorted(list(ground_y))[0]
+                print(player.rect.y)
             if event.key == pygame.K_SPACE:
-                if player.rect.y in ground_y:
+                if player.rect.y in ground_y and not jump_f:
                     ground = player.rect.y
+                    jumping_player.rect.x, jumping_player.rect.y = player.rect.x, player.rect.y
                     down_f = False
                     jump_f = True
 
     for spike in spikes_lst:
-        if pygame.sprite.collide_mask(player, spike):
-            STEP = 0
+        if pygame.sprite.collide_mask(player, spike) or pygame.sprite.collide_mask(jumping_player, spike):
             ALIVE = False
+            won = False
+            cur_state = 'dead_inside'
+            message_window_lose()
     for chest in chests_lst:
-        if pygame.sprite.collide_mask(player, chest):
+        if pygame.sprite.collide_mask(player, chest) or pygame.sprite.collide_mask(jumping_player, chest):
             ALIVE = False
             WIN = True
+            won = True
             cur_state = 'win'
             message_window_win()
     for coin in coins_lst:
-        if not pygame.sprite.collide_mask(player, coin):
+        if not pygame.sprite.collide_mask(player, coin) and not pygame.sprite.collide_mask(jumping_player, coin):
             screen.blit(coin.image, (coin.rect.x, coin.rect.y))
             coin.update()
         else:
@@ -336,22 +409,21 @@ while running:
             cur_coins += 1
 
     if ALIVE:
-        player.update()
-        camera.update(player)
         for sprite in all_sprites:
             camera.apply(sprite)
         if jump_f:
             jump()
+            screen.blit(jumping_player.image, (jumping_player.rect.x, jumping_player.rect.y))
+            camera.update(jumping_player)
         else:
+            screen.blit(player.image, (player.rect.x, player.rect.y))
+            player.update()
+            camera.update(player)
             player.rect.x += STEP
-
-    screen.blit(player.image, (player.rect.x, player.rect.y))
     clock.tick(FPS)
     pygame.display.flip()
     screen.fill((0, 0, 0))
     background_group.draw(screen)
-    tiles_group.draw(screen)
-    spikes_group.draw(screen)
-    chests_group.draw(screen)
+    level_group.draw(screen)
     coins_group.draw(screen)
 terminate()
